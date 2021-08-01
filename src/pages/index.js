45a69bf4706f549889b0cc9aab6815296b130871
Sudhanshu <sudhanshu221b@gmail.com>
2021-08-01 08:50:33 +0530

    initial commit with gatsby starter

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi people</h1>
+    <h2>I am <b>Sudhanshu.</b></h2>
+    <p>I am <code>JavaScript</code> developer.</p>
+  </Layout>
+)
+
+export default IndexPage
